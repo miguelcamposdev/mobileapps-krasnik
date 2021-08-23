e2775e0f1ed59c4171a49756392d208370a1be89
Miguel Campos <camposmiguel@gmail.com>
2021-08-23 09:21:56 +0200

    last changes

--- hello_world/lib/ui/main_page.dart
+++ hello_world/lib/ui/main_page.dart
@@ -1,4 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:hello_world/ui/login_page.dart';
 
 class MainPage extends StatefulWidget {
   @override
@@ -7,23 +10,36 @@ class MainPage extends StatefulWidget {
 
 class _MainPageState extends State<MainPage> {
   int _selectedIndex = 0;
+  List<Widget> _widgetPages;
+  FirebaseFirestore firestore = FirebaseFirestore.instance;
+  Stream<QuerySnapshot> _playersStream;
 
-  static const List<Widget> _widgetPages = <Widget>[
-    Text(
-      'List of football players',
-    ),
-    Text(
-      'List of football teams',
-    ),
-    Text(
-      'My profile',
-    ),
-  ];
+  _doLogout() async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(context).pushReplacement(
+      MaterialPageRoute(builder: (_) => LoginPage()),
+    );
+  }
 
   void _onItemTapped(int index) {
     setState(() {
       _selectedIndex = index;
     });
+  }
+
+  @override
+  void initState() {
+    _playersStream = firestore.collection('players').snapshots();
+    _widgetPages = <Widget>[
+      _playersListWidget(),
+      Text(
+        'List of football teams',
+      ),
+      Container(
+          child: ElevatedButton(
+              onPressed: () => _doLogout(), child: Text('Logout'))),
+    ];
+    super.initState();
   }
 
   @override
@@ -38,22 +54,57 @@ class _MainPageState extends State<MainPage> {
       bottomNavigationBar: BottomNavigationBar(
         items: const <BottomNavigationBarItem>[
           BottomNavigationBarItem(
-            icon: Icon(Icons.home),
-            label: 'Home',
+            icon: Icon(
+              Icons.recent_actors_outlined,
+            ),
+            label: 'Players',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.business),
-            label: 'Business',
+            icon: Icon(
+              Icons.local_police_outlined,
+            ),
+            label: 'Teams',
           ),
           BottomNavigationBarItem(
-            icon: Icon(Icons.school),
-            label: 'School',
+            icon: Icon(
+              Icons.account_circle_outlined,
+            ),
+            label: 'Profile',
           ),
         ],
         currentIndex: _selectedIndex,
         selectedItemColor: Colors.amber[800],
         onTap: _onItemTapped,
       ),
+    );
+  }
+
+  _playersListWidget() {
+    return StreamBuilder<QuerySnapshot>(
+      stream: _playersStream,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return ListView(
+          children: snapshot.data.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data = document.data() as Map<String, dynamic>;
+            return Row(
+              children: [
+                SizedBox(
+                  width: 100,
+                  child: Image(image: NetworkImage(data['photo'])),
+                ),
+                Text(data['name'])
+              ]);
+          }).toList(),
+        );
+      },
     );
   }
 }
